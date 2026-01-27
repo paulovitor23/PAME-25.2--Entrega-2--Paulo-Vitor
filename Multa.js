2d7ec90daca401057b582456ec60a8211e900f13
Paulo Vitor Couto Doederlein <pv@MacBook-Air-de-Paulo.local>
2026-01-27 13:45:43 -0300

    fix: mudanca de parametros do construtor da classe Multa

--- Multa.js
+++ Multa.js
@@ -1,12 +1,12 @@
 // Classe que representa uma multa aplicada a um condutor
 class Multa {
-    constructor(id, idCliente, tipo, valor, data, status) {
+    constructor(id, idCliente, tipo, valor, data, status = 'pendente') {
         this.id = id
         this.idCliente = idCliente
         this.tipo = tipo
         this.valor = valor
         this.data = data
-        this.status = status // pendente, paga, cancelada
+        this.status = status // pendente, paga, cancelada, recorrida
     }
 }
 
